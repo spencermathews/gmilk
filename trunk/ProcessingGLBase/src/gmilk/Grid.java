d67ba2670df6d8e40cb8b04b94e72b0826b48b15
23Flexi <23Flexi@52eb8436-093e-c876-3ded-f7a19989a084>
2010-07-21 20:53:57 +0000

    initial commit of the Milkdrop-esque grid warper

---
+++ trunk/ProcessingGLBase/src/gmilk/Grid.java
@@ -0,0 +1,59 @@
+package gmilk;
+
+public class Grid {
+	final private int gridSizeY, gridSizeX;
+	final private float[] nodesDx;
+	final private float[] nodesDy;
+
+	/**
+	 * The Grid-Type
+	 * @param sizeX - number of vertical  grid nodes
+	 * @param sizeY - number of horizontal grid nodes
+	 * @param Hscale - output scaling, horizontally
+	 * @param Vscale - output scaling, vertically
+	 */
+	public Grid(int sizeX, int sizeY) {
+		gridSizeX = sizeX;
+		gridSizeY = sizeY;
+		nodesDx = new float[sizeY * sizeX];
+		nodesDy = new float[sizeY * sizeX];
+	}
+	
+	public int getGridSizeX(){
+		return gridSizeX;
+	}
+	
+	public int getGridSizeY(){
+		return gridSizeY;
+	}
+	
+	final private int getIndex(int x, int y) {
+		return y * gridSizeX + x;
+	}
+
+	public float getX(int x, int y) {
+		return (float) x/(gridSizeX-1);
+	}
+	
+	public float getY(int x, int y) {
+		return (float) y/(gridSizeY-1);
+	}
+	
+	public float getDx(int x, int y){
+		return nodesDx[getIndex(x, y)];
+	}
+	
+	public float getDy(int x, int y){
+		return nodesDy[getIndex(x, y)];
+	}
+	
+	public float getWarpX(int x, int y){
+		return getX(x, y) + getDx(x, y) - 0.003f;
+	}
+	
+	public float getWarpY(int x, int y){
+		return getY(x, y) + getDy(x, y);
+	}
+	
+
+}
